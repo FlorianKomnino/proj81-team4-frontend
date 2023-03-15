f04e0b20d9d91e04e7750e42a991bb277b0cf4d2
FlorianKomnino <florian.komnino@outlook.it>
2023-03-15 16:30:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
