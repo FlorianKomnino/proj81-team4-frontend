397781af33e11332b9c4248e6b29083ce7028fa0
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-16 12:06:19 +0100

    import fortawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import { router } from './router'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas, far, fab)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
