585f5042cfa026fb1db76b7d904f3c26ff8f4b11
FlorianKomnino <florian.komnino@outlook.it>
2023-03-18 16:44:54 +0100

    create/add IconCard component

---
+++ src/components/IconCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="iconCard d-flex justify-content-center flex-wrap mx-3">
+        <font-awesome-icon class="icon" :icon="faIconString" />
+        <span>{{ iconArgument }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IconCard',
+    props: {
+        faIconString: String,
+        iconArgument: String,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.iconCard {
+    height: 4rem;
+    width: 4rem;
+
+    .icon {
+        font-size: 2rem;
+        margin: auto;
+    }
+
+    span {
+        width: 100%;
+        font-size: 0.5rem;
+        text-align: center;
+    }
+
+}
+</style>
