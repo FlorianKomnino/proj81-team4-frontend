f943ae03496cd5a0550040581fbcefaa098d21cb
Sebastiano Calella <sebastianocalella97@gmail.com>
2023-03-18 18:04:33 +0100

    create/add userDashboard component

--- src/pages/UserDashboard.vue
+++ src/pages/UserDashboard.vue
@@ -1,13 +1,51 @@
-<template lang="">
-    <div>
-        <h1>
-            ciao
-        </h1>
-    </div>
-</template>
 <script>
+import axios from 'axios';
+import DashboardComponent from '../components/DashboardComponent.vue'
+
 export default {
-    name: 'UserDashboard'
+    name: 'UserDashboard',
+    components:{
+        DashboardComponent
+    },
+    data() {
+        return {
+            name: '',
+            surname: '',
+            email: '',
+            password: '',
+            birth_date: '',
+            errors: {},
+            success: false,
+            apiAddress: 'http://127.0.0.1:8000/api/',
+        }
+    },
+    methods: {
+        getUserInfo(){
+            axios.get(`${this.apiAddress}user/1`, formData).then((response) => {
+                this.success = response.data.success;
+                console.log(this.success);
+                // console.warn(this.success);
+                if (this.success) {
+                    this.name = "";
+                    this.surname = "";
+                    this.email = "";
+                    this.password = "";
+                    this.birth_date = "";
+                } else {
+                    this.errors = response.data.errors;
+                    console.warn(this.errors);
+                }
+                this.loading = false;
+                //  se è andata bene fai a
+                // altrimenti fai b
+            })
+        }
+    },
 }
 </script>
+
+<template>
+    <DashboardComponent/>
+</template>
+
 <style lang="scss"></style>
