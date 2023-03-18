75100fa04960f36c6df9a84d1b8d44245ec187fa
FlorianKomnino <florian.komnino@outlook.it>
2023-03-18 16:46:22 +0100

    add IconCard v-for -- HomePage.vue page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,87 @@
 <template lang="">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1>
-                    Welcome to BoolBnB
-                </h1>
+            <div class="col-12 d-flex justify-content-between">
+                <IconCard v-for="icon in icons" :iconArgument="icon.title" :faIconString="icon.icon"/>
             </div>
         </div>
     </div>
 </template>
 <script>
+import IconCard from '../components/IconCard.vue'
 
 export default {
     name: 'HomePage',
     components: {
-    }
+        IconCard,
+    },
+
+    data() {
+        return {
+            icons: [
+                {
+                    title: 'In evidenza',
+                    icon: 'fa-solid fa-house-flag'
+                },
+                {
+                    title: 'Campings',
+                    icon: 'fa-solid fa-tents'
+                },
+                {
+                    title: 'Con piscina',
+                    icon: 'fa-solid fa-water-ladder'
+                },
+                {
+                    title: 'Nella natura',
+                    icon: 'fa-solid fa-tree'
+                },
+                {
+                    title: 'Vintage',
+                    icon: 'fa-solid fa-building-columns'
+                },
+                {
+                    title: 'Apartments',
+                    icon: 'fa-regular fa-building'
+                },
+                {
+                    title: 'Panorama',
+                    icon: 'fa-solid fa-panorama'
+                },
+                {
+                    title: 'Promozioni',
+                    icon: 'fa-solid fa-hand-holding-dollar'
+                },
+                {
+                    title: 'Montagna',
+                    icon: 'fa-solid fa-snowflake'
+                },
+                {
+                    title: 'Al mare',
+                    icon: 'fa-solid fa-water'
+                },
+                {
+                    title: 'Accesso ai disabili',
+                    icon: 'fa-solid fa-wheelchair'
+                },
+                {
+                    title: 'Parcheggio privato',
+                    icon: 'fa-solid fa-car'
+                },
+                {
+                    title: 'Regala un soggiorno',
+                    icon: 'fa-solid fa-gift'
+                },
+                {
+                    title: 'I più votati',
+                    icon: 'fa-regular fa-star'
+                },
+                {
+                    title: 'Preferiti',
+                    icon: 'fa-solid fa-heart'
+                },
+            ]
+        }
+    },
 
 }
 </script>
