ef282cecfd91391a93d50d9db6e4e825e0e95d76
FlorianKomnino <florian.komnino@outlook.it>
2023-03-19 18:32:30 +0100

    add router-Link to ApartmentsList

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,6 +11,7 @@ export default {
                 <img src="../assets/imgs/Boolbnb_md_logo.png" class="img-fluid" alt="Boolbnb logo">
             </div>
             <div class="col-3 justify-content-end d-flex">
+                <router-link :to="{name:'Apartments'}" class="btn btn-primary">Vai agli appartamenti</router-link>
                 <router-link :to="{name:'RegistrationForm'}" class="btn btn-primary">Sign up</router-link>
                 <router-link :to="{name:'LoginForm'}" class="btn btn-primary ms-2">Login</router-link>
             </div>
