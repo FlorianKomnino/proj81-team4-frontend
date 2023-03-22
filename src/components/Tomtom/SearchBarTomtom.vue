5083651ede821a4b5d2877ab96747cd0bb5c66c2
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-22 12:54:39 +0100

    modify form's css

--- src/components/Tomtom/SearchBarTomtom.vue
+++ src/components/Tomtom/SearchBarTomtom.vue
@@ -4,6 +4,7 @@ export default {
     data() {
         return {
             locationQuery: '',
+            radius: '',
         }
     }
 }
@@ -11,11 +12,19 @@ export default {
 
 <template lang="">
     <div class="container my-4">
-        <div class="row w-50">
+        <div class="row">
             <div class="input-group mb-3 col-3 p-0">
-                <input type="text" class="form-control shadow-none" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
-                placeholder="Search" v-model="locationQuery" @keyup.enter="$emit('location', locationQuery)">
-                <button href="#" @click="$emit('location', locationQuery)">Submit</button>
+                <form class="d-flex align-items-center" @keyup.enter="$emit('location', locationQuery, radius)">
+                    <div class="d-flex me-2 d-flex align-items-center">
+                        <label class="text-nowrap me-2">Inserisci una città:</label>
+                        <input type="text" class="form-control shadow-none" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+                        placeholder="Search" v-model="locationQuery">
+                    </div>
+                    <div class="d-flex d-flex align-items-center me-2">
+                        <label class="text-nowrap me-2">Inserisci un raggio (km):</label>
+                        <input type="number" class="form-control shadow-none" v-model="radius">
+                    </div>
+                </form>
             </div>
         </div>
     </div>
