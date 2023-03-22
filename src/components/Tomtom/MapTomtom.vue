6ef2dd4d50cae085c17bc082df171ebcf7486f2d
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-22 12:53:41 +0100

    implement dynamic radius

--- src/components/Tomtom/MapTomtom.vue
+++ src/components/Tomtom/MapTomtom.vue
@@ -70,6 +70,7 @@ export default {
                             lat: location.longitude,
                             lon: location.latitude,
                         },
+                        
                     })
                 });
                 console.log(this.apartments);
@@ -81,16 +82,14 @@ export default {
 
             }); 
         },
-        getHouses(locationQuery) {
+        getHouses(locationQuery, radius) {
+            console.log(locationQuery)
             axios.get(this.urlAddress + `${locationQuery}.json`, {
                 params: {
                     key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
                 }
             })
             .then((response) => {
-                console.log(response)
-                console.log(this.apartments)
-                console.log(this.apartmentsList)
                 let centerCoordinate = response.data.results[0].position;
                 let position = `${response.data.results[0].position.lon},${response.data.results[0].position.lat}`;
                 axios.get(this.positionUrlAddress, {
@@ -100,7 +99,7 @@ export default {
                         {
                         type: "CIRCLE",
                         position: position,
-                        radius: 10000
+                        radius: radius*1000
                         }
                     ]),
                     poiList: JSON.stringify(this.apartments)
@@ -157,54 +156,6 @@ export default {
                 })
             map.addControl(new tt.FullscreenControl());
             map.addControl(new tt.NavigationControl());
-        },
-        getCoordinate(locationQuery) {
-            axios.get(this.urlAddress + `${locationQuery}.json`, {
-                params: {
-                    key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
-                }
-            })
-            .then((response) => {
-                console.log(response.data.results[0]);
-                const map = tt.map({
-                key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
-                container: "map",
-                center: response.data.results[0].position,
-                zoom: 15
-                })
-                map.on('load', () => {
-                    const iconMarker = document.getElementById('marker');
-                    let customPopup = document.createElement('div');
-                    customPopup.classList.add("custom-popup");
-                    customPopup.innerHTML = '<p style="font-size: 0.9rem" class="m-0 me-2">Posizione esatta fornita dopo la prenotazione.</p>'
-                    let popupOffsets = {
-                    top: [0, 0],
-                    bottom: [0, -50],
-                    }
-                    let popup = new tt.Popup({ offset: popupOffsets, className: 'custom-popup'}).setDOMContent(customPopup);
-                    new tt.Marker({element: iconMarker}).setLngLat(response.data.results[0].position).addTo(map).setPopup(popup).togglePopup()
-                })
-                map.addControl(new tt.FullscreenControl());
-                map.addControl(new tt.NavigationControl());
-                this.map = Object.freeze(map);
-            })
-            .catch(function (error) {
-                console.log(error);
-            })
-            .finally(function () {
-
-            }); 
-            /* let map = tt.services.fuzzySearch({
-                key: 'jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr',
-                query: 'milano' //The input gotten from the search
-            }).go().then(function (response) {
-                map = tt.map({
-                    key: API_KEY,
-                    container: 'map-div',
-                    center: response.results[0].position,
-                    zoom: 12
-                });
-            }) */
         }
     },
     mounted(){
