289f4902b763547400bd05df0b9cd173e5be1dbb
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-03-22 21:24:33 +0100

    implement results after user location search

--- src/components/Tomtom/MapTomtom.vue
+++ src/components/Tomtom/MapTomtom.vue
@@ -5,6 +5,7 @@ import tt from '@tomtom-international/web-sdk-maps'
 import FuzzySearch from 'fuzzy-search';
 
 import SearchBarTomtom from './SearchBarTomtom.vue';
+import ApartmentCard from '../ApartmentCard.vue';
 import axios from 'axios';
 import { Marker } from 'mapbox-gl';
 
@@ -12,68 +13,45 @@ export default {
     name: 'MapTomtom',
     components: {
         SearchBarTomtom,
+        ApartmentCard,
     },
     data(){
         return{
+            locationQuery: '',
             apartments: [],
+            initialApartments: [],
+            foundApartments: [],
+            apartmentsToShow: [],
             loading: false,
             apartmentsUrlAddress: 'http://127.0.0.1:8000/api/apartments',
             urlAddress: 'https://api.tomtom.com/search/2/search/',
             positionUrlAddress: 'https://api.tomtom.com/search/2/geometryFilter.json',
-            apartmentsList: [
-                {
-                    position: {
-                        lat: 9.165437,
-                        lon: 45.464718,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.163100,
-                    lon: 45.478681,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.207381,
-                    lon: 45.483141,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.677238,
-                    lon: 45.698168,
-                    }
-                },
-                {
-                    position: {
-                    lat: 9.661306,
-                    lon: 45.682499,
-                    }
-                }
-            ]
         }
     },
     methods: {
         getApartments(){
+            this.locationQuery = '';
+            this.apartmentsToShow = [];
+            this.apartments = [];
             axios.get(this.apartmentsUrlAddress, {
                 params: {
 
                 }
             })
             .then((response) => {
-                console.log(response.data.results)
+                this.initialApartments = response.data.results;
+                this.apartmentsToShow = response.data.results;
                 let results = response.data.results;
                 results.forEach(location => {
                     this.apartments.push({
                         position: {
-                            lat: location.longitude,
-                            lon: location.latitude,
+                            lat: location.latitude,
+                            lon: location.longitude,
+                            id: location.id
                         },
                         
                     })
                 });
-                console.log(this.apartments);
             })
             .catch(function (error) {
                 console.log(error);
@@ -83,7 +61,8 @@ export default {
             }); 
         },
         getHouses(locationQuery, radius) {
-            console.log(locationQuery)
+            this.locationQuery = locationQuery.charAt(0).toUpperCase() + locationQuery.slice(1);
+            this.apartmentsToShow = [];
             axios.get(this.urlAddress + `${locationQuery}.json`, {
                 params: {
                     key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
@@ -91,7 +70,7 @@ export default {
             })
             .then((response) => {
                 let centerCoordinate = response.data.results[0].position;
-                let position = `${response.data.results[0].position.lon},${response.data.results[0].position.lat}`;
+                let position = `${response.data.results[0].position.lat}, ${response.data.results[0].position.lon}`;
                 axios.get(this.positionUrlAddress, {
                 params: {
                     key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
@@ -106,30 +85,34 @@ export default {
                 }
                 })
                 .then((response) => {
-                    console.log(response)
                     const map = tt.map({
                     key: "jEFhMI0rD5tTkGjuW8dYlC2x3UFxNRJr",
                     container: "map",
                     center: centerCoordinate,
-                    zoom: 13
+                    zoom: 11
                     })
                     map.on('load', () => {
-                        /* const MarkerEl = document.createElement("div");
-                        const iconMarker = document.createElement("div");
-                        iconMarker.innerHTML(iconMarker);
-                        MarkerEl.classList.add('marker');
-                        console.log(MarkerEl); */
+                        this.foundApartments = response.data.results
                         response.data.results.forEach(function (location) {
                             console.log(location)
-                            let marker = new tt.Marker().setLngLat([location.position.lat, location.position.lon]).addTo(map) 
+                            const MarkerEl = document.createElement("div");
+                            MarkerEl.classList.add('marker', 'd-flex', 'justify-content-center', 'align-items-center');
+                            MarkerEl.innerHTML = "<font-awesome-icon :icon=\"['fas', 'house']\" />";
+                            let marker = new tt.Marker().setLngLat([location.position.lon, location.position.lat]).addTo(map) 
                             const popup = new tt.Popup({ anchor: 'top' }).setText('Posizione esatta fornita dopo la prenotazione.')
                             marker.setPopup(popup)
                         })
-                        
+                        for(let outerI=0 ; outerI<this.foundApartments.length; outerI++ ){
+                            for(let innerI=0 ; innerI<this.initialApartments.length; innerI++ ){
+                                if(this.foundApartments[outerI].position.id == this.initialApartments[innerI].id){
+                                    console.log('presente')
+                                    this.apartmentsToShow.push(this.initialApartments[innerI])
+                                }
+                            }
+                        }
                     })
                     map.addControl(new tt.FullscreenControl());
                     map.addControl(new tt.NavigationControl());
-                    this.map = Object.freeze(map);
                 })
                 .catch(function (error) {
                     console.log(error);
@@ -140,7 +123,7 @@ export default {
             })
             
         },
-        map(){
+        initialMap(){
             const iconMarker = document.getElementById('marker');
             let center = [9.192771, 45.463273]
             const map = tt.map({
@@ -150,16 +133,14 @@ export default {
             zoom: 12
             })
             map.on('load', () => {
-                    const iconMarker = document.getElementById('marker');
-                    let marker = new tt.Marker({element: iconMarker}).setLngLat(center);
-                    marker.addTo(map);
+                    
                 })
             map.addControl(new tt.FullscreenControl());
             map.addControl(new tt.NavigationControl());
         }
     },
     mounted(){
-        this.map();
+        this.initialMap();
     },
     created(){
         this.getApartments();
@@ -168,26 +149,67 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container">
-        <SearchBarTomtom @location="getHouses"/>
-        <div id="map" class="map">
-            <div id="marker" class="d-flex justify-content-center align-items-center"><font-awesome-icon icon="fa-solid fa-house" class="icon"/></div>
+    <hr class="m-0 mb-3">
+    <div class="main-container">
+        <div class="container-fluid search-bar-container">
+            <SearchBarTomtom @location="getHouses"/>
+        </div>
+        <div class="container-fluid pe-0">
+            <div class="d-flex cards-map-container">
+                <div class="card-container pe-4" v-if="apartmentsToShow">
+                    <p v-if="locationQuery">
+                        Abbiamo trovato {{apartmentsToShow.length}} alloggi in questa località: {{locationQuery}}
+                    </p>
+                    <div class="row g-4 align-items-stretch" >
+                        <div class="col-4" v-for="apartment in apartmentsToShow">
+                            <ApartmentCard :title="apartment.title" :image="apartment.image" :rooms="apartment.rooms" :beds="apartment.beds" :address="apartment.address"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="map-container">
+                    <div id="map" class="map"></div>
+                </div>
+            </div>
         </div>
     </div>
+        
+    
 </template>
 
 <style lang="scss" scoped>
 @use "../../styles/general.scss" as *;
 
+hr{
+    border: 0;
+    border-top: 1px solid lightgray;
+}
+
+.main-container{
+
+    position: fixed;
+
+    .cards-map-container{
+    height: 86vh;
+    overflow-y: scroll;
+    
+
+    .card-container{
+    width: 60vw;
+    
+}
+
+.map-container{
+    width: 40vw;
+    position: fixed;
+    right: 0;
     #map{
-        height: 500px;
-        .custom-popup{
-            border-radius: 20px;
-            font-size: 100rem;
-        }
+        height: 100vh;
         .marker{
-            background-color: $main-bg-color;
+            background-color: black;
             border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            padding: 10px;
             .icon{
                 color: white;
                 font-size: 1.3rem;
@@ -196,4 +218,11 @@ export default {
         }
 
     }
+}
+}
+
+}
+
+
+    
 </style>
