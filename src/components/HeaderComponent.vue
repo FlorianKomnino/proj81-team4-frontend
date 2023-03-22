44bc0377e88614cf01771ce5f68b6d5c6992ecee
FlorianKomnino <florian.komnino@outlook.it>
2023-03-22 21:50:27 +0100

    modify urls for login and logout

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -11,32 +11,41 @@ export default {
                 <img src="../assets/imgs/Boolbnb_md_logo.png" class="img-fluid logo p-0" alt="Boolbnb logo">
             </div>
             <div class="col-3 justify-content-end d-flex">
-                <button>
+                <a href="http://127.0.0.1:8000/login">
                     Accedi
-                </button>
-                <router-link :to="{name:'RegistrationForm'}" class="btn btn-primary">Sign up</router-link>
-                <router-link :to="{name:'LoginForm'}" class="btn btn-primary ms-2">Login</router-link>
+                </a>
+                <a href="http://127.0.0.1:8000/register">
+                    Registrati
+                </a>
             </div>
         </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-    .header-container{
-        height: 8vh;
-        position: fixed;
-        z-index: 2;
+.header-container {
+    height: 8vh;
+    position: fixed;
+    z-index: 2;
 
-        button{
-            background-color: white;
-            border: 1px solid lightgray;
+    a {
+        background-color: white;
+        padding: 10px;
+        border: 1.5px solid transparent;
+        text-decoration: none;
+        color: black;
+        margin: 1rem;
+
+        &:hover {
+            border: 1.5px solid black;
             border-radius: 30px;
-            padding: 10px;
-        }
 
-        .logo{
-            height: 100%;
-            transform: scale(0.7);
         }
     }
+
+    .logo {
+        height: 100%;
+        transform: scale(0.7);
+    }
+}
 </style>
