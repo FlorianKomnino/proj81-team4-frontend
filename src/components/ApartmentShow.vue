90ef128ea15888a69b6e7168b5973829758d9e6c
CastellucciLuca <castellucciluca98@gmail.com>
2023-03-27 15:53:37 +0200

    Add Apartment Title-image-numbers of rooms/beds and bathrooms in ApartmentShow

--- src/components/ApartmentShow.vue
+++ src/components/ApartmentShow.vue
@@ -2,7 +2,7 @@
 export default {
     data() {
         return {
-
+            imageBaseURL: 'http://127.0.0.1:8000/',
         }
     },
     props: {
@@ -13,7 +13,13 @@ export default {
 <template lang="">
     <div class='container'>
         <div class='col-12'>
-            <h3>{{apartment.title}}</h3>
+            <h2>{{apartment.title}}</h2>
+            <h6>{{apartment.address}}</h6>
+            <img :src="imageBaseURL+'storage/'+apartment.image" alt="" class='rounded mx-auto d-block'>
+        <div class='mt-5 col-8'></div>
+            <span> Numero di stanze: {{apartment.rooms}} &#8226;</span>
+            <span> Numero di letti: {{apartment.beds}} &#8226;</span>
+            <span> Numero di bagni: {{apartment.bathrooms}}</span>
         </div>
     </div>
 </template>
