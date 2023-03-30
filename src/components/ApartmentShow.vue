4d7cb32c3659ef81895088e82def48175e5f5624
LucaArcudi <arcudilu@gmail.com>
2023-03-30 13:52:19 +0200

    implement email validation on messages form

--- src/components/ApartmentShow.vue
+++ src/components/ApartmentShow.vue
@@ -12,9 +12,11 @@ export default {
             latitude: 0,
             userMessage: '',
             userEmail: '',
+            validatedUserEmail: '',
             apartmentId: '',
             userName: '',
             formData: {},
+            emailErrorMessages: ''
         }
     },
     props: {
@@ -39,53 +41,55 @@ export default {
             map.addControl(new tt.FullscreenControl());
             map.addControl(new tt.NavigationControl());
         },
-        sendMessage() {
-            const innerFormData = {
-                text_content: this.formData['userMessage'],
-                email: this.formData['userEmail'],
-                apartment_id: this.formData['apartmentId'],
-                name: this.formData['name'],
-            };
-            axios.post(this.messageBaseUrl, innerFormData)
-            .then( (response) => {
-                console.log(response);
-                this.$toast.success(`Messaggio inviato con successo`);
-            })
-            .catch((error) => {
-                console.log(error);
-                this.$toast.error(`Messaggio non inviato`);
-            })
-        },
-        /*.then((response) => {
-                    console.log(response);
-                    this.success = response.data.success;
-                    if (this.success) {
-                        this.$toast.success(`Messaggio inviato con successo`);
-                    } else {
-                        this.errors = response.data.errors;
-                        console.warn(this.errors);
-                        this.$toast.error(`Messaggio non inviato`);
-                    }*/
 
-        /*
-        *
-        * return: void 
-        * 
-        */
         getApartmentId() {
             this.apartmentId = document.getElementById('apartmentId').value;
-            console.log(this.apartmentId);
         },
+
+        emailValidation() {
+            if (!this.userEmail) {
+                this.emailErrorMessages = 'L\'email è necessaria';
+            } else {
+                let regex = /\S+@\S+.\S+/;
+                if (!regex.test(this.userEmail)) {
+                    this.emailErrorMessages = 'Email non valida';
+                } else {
+                    this.emailErrorMessages = '';
+                    this.validatedUserEmail = this.userEmail;
+                }
+            }
+        },
+
         getFormData() {
             this.formData['userMessage'] = this.userMessage;
-            this.formData['userEmail'] = this.userEmail;
+            this.formData['validatedUserEmail'] = this.validatedUserEmail;
             this.formData['apartmentId'] = this.apartmentId;
             this.formData['name'] = this.userName;
 
             console.log(this.formData);
         },
+
+        sendMessage() {
+            const innerFormData = {
+                text_content: this.formData['userMessage'],
+                email: this.formData['validatedUserEmail'],
+                apartment_id: this.formData['apartmentId'],
+                name: this.formData['name'],
+            };
+            axios.post(this.messageBaseUrl, innerFormData)
+                .then((response) => {
+                    console.log(response);
+                    this.$toast.success(`Messaggio inviato con successo`);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    this.$toast.error(`Messaggio non inviato`);
+                })
+        },
+
         getAndSendFormData() {
             this.getApartmentId();
+            this.emailValidation();
             this.getFormData();
             this.sendMessage();
             this.userEmail = '';
@@ -97,7 +101,6 @@ export default {
     mounted() {
         this.initialMap();
         this.getApartmentId();
-        this.getFormData();
     }
 }
 </script>
@@ -106,14 +109,14 @@ export default {
         <div class="row mb-4">
             <div class='col-12'>
                 <input type="text" name="apartmentId" class="d-none" :value="apartment.id" id="apartmentId">
-                <h2 class="fs-2">{{apartment.title[0].toUpperCase() + apartment.title.slice(1)}}</h2>
-                <h6 class="text-decoration-underline">{{apartment.address}}</h6>
+                <h2 class="fs-2">{{ apartment.title[0].toUpperCase() + apartment.title.slice(1) }}</h2>
+                <h6 class="text-decoration-underline">{{ apartment.address }}</h6>
             </div>
         </div>
         <div class="row g-1 imgMap">
             <div class="col-6 d-flex m-0 img-wrapper">
                 <img v-if="image.startsWith('http')" :src="image" class="rounded-3 img-fluid" alt="image">
-                <img v-else :src="imageBaseURL+'storage/'+image"  class='rounded-3 img-fluid' alt="image">
+                <img v-else :src="imageBaseURL + 'storage/' + image" class='rounded-3 img-fluid' alt="image">
             </div>
             <div class="col-6 m-0 d-flex align-items-center">
                 <div id="map" class="rounded-3">
@@ -126,26 +129,31 @@ export default {
         <div class="row mt-5">
             <div class="col-7 pe-5">
                 <p class="mb-0 fs-5">
-                    Intero Alloggio: <span class="fw-bold">{{apartment.title[0].toUpperCase() + apartment.title.slice(1)}}</span> - Host: Inserire Host
+                    Intero Alloggio: <span class="fw-bold">{{ apartment.title[0].toUpperCase() +
+                        apartment.title.slice(1) }}</span> - Host: Inserire Host
                 </p>
                 <p class="fs-6 fw-light">
-                    <span> Numero di stanze: {{apartment.rooms}} &#8226;</span>
-                    <span> Numero di letti: {{apartment.beds}} &#8226;</span>
-                    <span> Numero di bagni: {{apartment.bathrooms}}</span>
+                    <span> Numero di stanze: {{ apartment.rooms }} &#8226;</span>
+                    <span> Numero di letti: {{ apartment.beds }} &#8226;</span>
+                    <span> Numero di bagni: {{ apartment.bathrooms }}</span>
                 </p>
                 <hr>
                 <div class='mt-4'>
                     <h3>Cosa troverai</h3>
                     <p v-for="service in apartment.services">
                         <font-awesome-icon :icon="service.icon" />
-                        {{service.name}}                
+                        {{ service.name }}
                     </p>
                 </div>
             </div>
             <div class="col-4 offset-1">
                 <div class="messageContainer rounded-3 access-buttons text-center p-5">
                     <div class="d-flex flex-column text-start mb-1">
-                        <label for="">Email:</label>
+                        <label for="">Email:
+                            <span id="email-error" class="text-danger">
+                                {{ emailErrorMessages }}
+                            </span>
+                        </label>
                         <input ref="userEmail" type="email" v-model="userEmail">
                     </div>
                     <div class="d-flex flex-column text-start mb-1">
@@ -170,34 +178,35 @@ export default {
 <style lang="scss" scoped>
 @use "../styles/general.scss" as *;
 
-.imgMap{
+.imgMap {
     max-height: 420px;
 
-    .img-wrapper{
+    .img-wrapper {
         max-height: 420px;
-        img{
+
+        img {
             margin: auto;
             height: 100%;
         }
     }
 
     #map {
-    height: 420px;
-    width: 100%;
+        height: 420px;
+        width: 100%;
 
-    #marker {
-        background-color: $main-bg-color;
-        border-radius: 50%;
-        height: 48px;
-        width: 48px;
-        color: white;
+        #marker {
+            background-color: $main-bg-color;
+            border-radius: 50%;
+            height: 48px;
+            width: 48px;
+            color: white;
 
-        .marker-icon {
-            height: 22px;
-            width: 22px;
+            .marker-icon {
+                height: 22px;
+                width: 22px;
+            }
         }
     }
-}
 
 }
 
@@ -205,23 +214,28 @@ export default {
 .messageContainer {
     height: 450px;
     box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    input{
-        background-color: rgb(241,243,246);
+
+    input {
+        background-color: rgb(241, 243, 246);
         height: 40px;
         padding: 15px;
-        &:focus{
+
+        &:focus {
             outline-color: rgba(255, 55, 92, 0.75);
         }
     }
-    textarea{
+
+    textarea {
         max-height: 100px;
         padding: 15px;
-        background-color: rgb(241,243,246);
-        &:focus{
+        background-color: rgb(241, 243, 246);
+
+        &:focus {
             outline-color: rgba(255, 55, 92, 0.75);
         }
     }
-    a#send-message{
+
+    a#send-message {
         background: linear-gradient(108deg, white 0 45%, $main-bg-color 45% 90%, white 90% 100%);
         background-size: 190%;
         width: 100px;
@@ -233,6 +247,7 @@ export default {
         box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
         cursor: pointer;
         transition: all .3s;
+
         &:hover {
             color: white;
             background-position: right;
